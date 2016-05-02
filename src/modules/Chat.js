b8c35b4cd2160f22cb30fead80cedb5ffa43d276
jinmayamashita <jinmayamashita@gmail.com>
2016-05-02 13:50:31 +0900

    build ChatForm component should ask the user for their msg and send a request to the server

--- src/modules/Chat.js
+++ src/modules/Chat.js
@@ -3,11 +3,17 @@ import ChatList from './ChatList'
 import ChatForm from './ChatForm'
 
 export default class Chat extends Component {
+  constructor (props) {
+    super(props)
+  }
+  _handleMessageSubmit (msg) {
+    console.log(msg)
+  }
   render () {
     return (
       <div>
         <ChatList/>
-        <ChatForm/>
+        <ChatForm onCommentSubmit={this._handleMessageSubmit.bind(this)} />
       </div>
     )
   }
--- src/modules/ChatForm.js
+++ src/modules/ChatForm.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 export default class ChatForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { msg: '' }
+  }
+  _handleChange (e) {
+    this.setState({
+      msg: e.target.value
+    })
+  }
+  _handleKeyPress (e) {
+    if (e.key === 'Enter') {
+      let msg = this.state.msg.trim()
+      this.props.onCommentSubmit({
+        msg: msg
+      })
+      this.setState({ msg: '' })
+    }
+  }
   render () {
     return (
       <div className='comment-form'>
-        <input type='text' placeholder='message here' />
+        <input
+          type='text'
+          placeholder='message here'
+          onChange={this._handleChange.bind(this)}
+          value={this.state.msg}
+          onKeyPress= {this._handleKeyPress.bind(this)}
+        />
       </div>
     )
   }
+}
+
+ChatForm.propTypes = {
+  onCommentSubmit: PropTypes.func.isRequired
 }
